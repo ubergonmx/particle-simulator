6da318c01b921251b88fd3e98c5b26cda367e054
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-28 00:59:13 +0800

    Refactor app size in

--- particle-simulator/MyForm.h
+++ particle-simulator/MyForm.h
@@ -1,6 +1,7 @@
 #pragma once
 
-namespace particlesimulator {
+namespace particlesimulator
+{
 
 	using namespace System;
 	using namespace System::ComponentModel;
@@ -12,20 +13,20 @@ namespace particlesimulator {
 	/// <summary>
 	/// Summary for MyForm
 	/// </summary>
-	public ref class MyForm : public System::Windows::Forms::Form
+public
+	ref class MyForm : public System::Windows::Forms::Form
 	{
-		Graphics^ g;
+		Graphics ^ g;
 		bool cursorMoving = false;
-		Pen^ cursorPen;
+		Pen ^ cursorPen;
 		int cursorX, cursorY = -1;
-
 
 	public:
 		MyForm(void)
 		{
 			InitializeComponent();
 			//
-			//TODO: Add the constructor code here
+			// TODO: Add the constructor code here
 			//
 			g = canvas->CreateGraphics();
 			cursorPen = gcnew Pen(Color::Black);
@@ -42,17 +43,20 @@ namespace particlesimulator {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Panel^ canvas;
-	private: System::Windows::Forms::Label^ titleLbl;
-	protected:
+
+	private:
+		System::Windows::Forms::Panel ^ canvas;
+
+	private:
+		System::Windows::Forms::Label ^ titleLbl;
 
 	protected:
-
+	protected:
 	private:
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
-		System::ComponentModel::Container ^components;
+		System::ComponentModel::Container ^ components;
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -81,7 +85,7 @@ namespace particlesimulator {
 			this->titleLbl->AutoSize = true;
 			this->titleLbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->titleLbl->Location = System::Drawing::Point(1305, 9);
+			this->titleLbl->Location = System::Drawing::Point(1286, 9);
 			this->titleLbl->Name = L"titleLbl";
 			this->titleLbl->Size = System::Drawing::Size(398, 55);
 			this->titleLbl->TabIndex = 1;
@@ -89,9 +93,8 @@ namespace particlesimulator {
 			// 
 			// MyForm
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(9, 20);
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1728, 760);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			this->ClientSize = System::Drawing::Size(1706, 704);
 			this->Controls->Add(this->titleLbl);
 			this->Controls->Add(this->canvas);
 			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
@@ -102,23 +105,32 @@ namespace particlesimulator {
 
 		}
 #pragma endregion
-	
-	private: System::Void canvas_MouseDown(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-		cursorMoving = true;
-		cursorX = e->X;
-		cursorY = e->Y;
-	}
-	private: System::Void canvas_MouseUp(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-		cursorMoving = false;
-		cursorX = -1;
-		cursorY = -1;
-	}
-	private: System::Void canvas_MouseMove(System::Object^ sender, System::Windows::Forms::MouseEventArgs^ e) {
-		if (cursorX != 1 && cursorY != -1 && cursorMoving) {
-			g->DrawLine(cursorPen, cursorX, cursorY, e->X, e->Y);
+
+	private:
+		System::Void canvas_MouseDown(System::Object ^ sender, System::Windows::Forms::MouseEventArgs ^ e)
+		{
+			cursorMoving = true;
 			cursorX = e->X;
 			cursorY = e->Y;
 		}
-	}
+
+	private:
+		System::Void canvas_MouseUp(System::Object ^ sender, System::Windows::Forms::MouseEventArgs ^ e)
+		{
+			cursorMoving = false;
+			cursorX = -1;
+			cursorY = -1;
+		}
+
+	private:
+		System::Void canvas_MouseMove(System::Object ^ sender, System::Windows::Forms::MouseEventArgs ^ e)
+		{
+			if (cursorX != 1 && cursorY != -1 && cursorMoving)
+			{
+				g->DrawLine(cursorPen, cursorX, cursorY, e->X, e->Y);
+				cursorX = e->X;
+				cursorY = e->Y;
+			}
+		}
 	};
 }
