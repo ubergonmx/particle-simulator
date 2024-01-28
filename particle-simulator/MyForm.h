bc49cbed5f731ae07ce93607a77028fc39d00bb6
aaron <aaron_palpallatoc@dlsu.edu.ph>
2024-01-28 14:45:23 +0800

    Update MyForm.h with new labels and clear button

--- particle-simulator/MyForm.h
+++ particle-simulator/MyForm.h
@@ -25,7 +25,11 @@ public
 
 		Rectangle dvdLogo;
 		Point velocity;
-		Color dvdLogoColor;
+	private: System::Windows::Forms::Label^ titleLbl;
+	private: System::Windows::Forms::Button^ clearBtn;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+		   Color dvdLogoColor;
 
 	public:
 		MyForm(void)
@@ -38,7 +42,7 @@ public
 			cursorPen = gcnew Pen(Color::Black);
 
 			// Initialize the DVD logo and its velocity
-			dvdLogo = Rectangle(0, 0, 100, 50);
+			dvdLogo = Rectangle(0, 0, 20, 20);
 			dvdLogoColor = Color::Red;
 			velocity = Point(5, 5);
 
@@ -105,7 +109,7 @@ public
 		System::Windows::Forms::Panel ^ canvas;
 
 	private:
-		System::Windows::Forms::Label ^ titleLbl;
+
 
 	protected:
 	protected:
@@ -124,46 +128,84 @@ public
 		{
 			this->canvas = (gcnew System::Windows::Forms::Panel());
 			this->titleLbl = (gcnew System::Windows::Forms::Label());
+			this->clearBtn = (gcnew System::Windows::Forms::Button());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
-			//
+			// 
 			// canvas
-			//
+			// 
 			this->canvas->BackColor = System::Drawing::SystemColors::GradientActiveCaption;
 			this->canvas->Location = System::Drawing::Point(0, 0);
 			this->canvas->Name = L"canvas";
-			this->canvas->Size = System::Drawing::Size(800, 640);
+			this->canvas->Size = System::Drawing::Size(582, 440);
 			this->canvas->TabIndex = 0;
 			this->canvas->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &MyForm::canvas_Paint);
 			this->canvas->MouseDown += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::canvas_MouseDown);
 			this->canvas->MouseMove += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::canvas_MouseMove);
 			this->canvas->MouseUp += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::canvas_MouseUp);
-			//
+			// 
 			// titleLbl
-			//
+			// 
 			this->titleLbl->AutoSize = true;
-			this->titleLbl->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-																static_cast<System::Byte>(0)));
-			this->titleLbl->Location = System::Drawing::Point(815, 9);
+			this->titleLbl->Font = (gcnew System::Drawing::Font(L"Segoe UI", 24, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->titleLbl->Location = System::Drawing::Point(588, 9);
 			this->titleLbl->Name = L"titleLbl";
-			this->titleLbl->Size = System::Drawing::Size(398, 55);
+			this->titleLbl->Size = System::Drawing::Size(397, 65);
 			this->titleLbl->TabIndex = 1;
 			this->titleLbl->Text = L"Particle Simulator";
-			//
+			// 
+			// clearBtn
+			// 
+			this->clearBtn->Location = System::Drawing::Point(604, 352);
+			this->clearBtn->Name = L"clearBtn";
+			this->clearBtn->Size = System::Drawing::Size(254, 62);
+			this->clearBtn->TabIndex = 2;
+			this->clearBtn->Text = L"Clear drawings";
+			this->clearBtn->UseVisualStyleBackColor = true;
+			this->clearBtn->Click += gcnew System::EventHandler(this, &MyForm::clearBtn_Click);
+			// 
+			// label1
+			// 
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(598, 88);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(388, 148);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Using .NET Framework (WinForms) to simulate physics.";
+			// 
+			// label2
+			// 
+			this->label2->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label2->Location = System::Drawing::Point(598, 281);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(353, 68);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"You can paint on the canvas (blue area) to draw walls";
+			// 
 			// MyForm
-			//
-			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::None;
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(144, 144);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Dpi;
 			this->AutoScroll = true;
-			this->ClientSize = System::Drawing::Size(1258, 644);
+			this->ClientSize = System::Drawing::Size(999, 440);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->clearBtn);
 			this->Controls->Add(this->titleLbl);
 			this->Controls->Add(this->canvas);
 			this->Font = (gcnew System::Drawing::Font(L"Segoe UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-													  static_cast<System::Byte>(0)));
+				static_cast<System::Byte>(0)));
 			this->Margin = System::Windows::Forms::Padding(3, 4, 3, 4);
 			this->Name = L"MyForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Particle Simulator - Mangoba, Palpallatoc";
 			this->ResumeLayout(false);
 			this->PerformLayout();
+
 		}
 #pragma endregion
 
@@ -209,6 +251,10 @@ public
 			// Draw the DVD logo
 			g->FillRectangle(gcnew SolidBrush(dvdLogoColor), dvdLogo);
 		}
-	};
+
+private: System::Void clearBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+	bitmap = gcnew Bitmap(canvas->Width, canvas->Height);
+}
+};
 
 }
